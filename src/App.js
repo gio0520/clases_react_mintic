8f990d5df57d09c56ef8dc1a6178751c0f502019
sergio <gioprogramacionweb@gmail.com>
2021-09-23 07:45:42 -0500

    Instalacion React 1

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import logo from './media/logo.png';
+import './styles/styles.css';
+import bordercollie from './media/borderCollie.jpg'
+import rhodesian from './media/rhodesian.jpg'
+
+// CLASE VIDEO 21 DE SEPTIEMBRE --> INSTALACION Node, JARN, REACTJS
+// CLASE VIDEO 21 DE SEPTIEMBRE --> INSTALACION Node, JARN, REACTJS
+// CLASE VIDEO 21 DE SEPTIEMBRE --> INSTALACION Node, JARN, REACTJS
+// CLASE VIDEO 21 DE SEPTIEMBRE --> INSTALACION Node, JARN, REACTJS
+function App() {
+  return (
+    <div className="App">
+     <header>
+      <ul className="navbar">
+        <li>
+          <img src={logo} alt="imagen" className="logo" />
+        </li>
+        <li>
+          <button className="botonGenerico mainButton">Nueva Post</button>
+        </li>
+        <li>
+          <div className="buscar">
+            <input placeholder="Buscar una raza" />
+            <i className="fas fa-search botonGenerico iconoBusqueda"></i>
+          </div>
+        </li>
+        <li><button className="botonGenerico secondaryButton">Login</button></li>
+        <li><button className="botonGenerico mainButton">Registro</button></li>
+      </ul>
+    </header>
+    <main>
+      <section>
+        <h1>Razas de Perros</h1>
+        <ul className="breedCardContainer">
+          <li className="breedCard">
+            <div className="contenedorImagen">
+              <img src={bordercollie} alt="Border Collie" />
+            </div>
+            <span className="breedTitle">Border Collie </span>
+          </li>
+          <li className="breedCard">
+            <div className="contenedorImagen">
+              <img src={rhodesian}  alt="Rhodesian" />
+            </div>
+            <span className="breedTitle">Rhodesian</span>
+          </li>
+        </ul>
+      </section>
+      <section></section>
+    </main>
+    <footer></footer>
+    </div>
+  );
+}
+
+export default App;
